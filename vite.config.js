8e0898d12152cfd6db8e2ca3d176e8e3c832b4b1
atosugiharto <atosoegiharto@gmail.com>
2025-01-22 17:47:33 +0700

    add config alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // Pastikan modul 'path' diimport
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.PNG'],
-})
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+    },
+  },
+  assetsInclude: ["**/*.png", "**/*.PNG"],
+});
